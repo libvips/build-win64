6f54fee054ffaa01947675d94290004dc83999cb
John Cupitt <jcupitt@gmail.com>
2016-08-18 14:49:12 +0100

    add an 8.4 dir

---
+++ 8.4/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# the whole build process
+
+. variables.sh
+
+# export this to vips.modules ... cmake needs it
+export BASEDIR=$basedir
+
+vips_site=http://www.vips.ecs.soton.ac.uk/supported/$vips_version
+
+# basic setup ... must do this before the native build, since it'll wipe the
+# install area
+./get-win64-packages.sh && \
+  ./unpack.sh && 
+
+# we need to do a native linux build to generate a typelib --
+# the gobject-introspection machinery won't work in a cross-compiler
+if [ ! -d $linux_install ]; then
+  ( mkdir native-linux-build; cd native-linux-build; \
+    vips_name=$vips_package-$vips_version.$vips_minor_version
+    wget $vips_site/${vips_name}.tar.gz &&
+    tar xf ${vips_name}.tar.gz &&
+    cd $vips_name &&
+    CFLAGS="-g" CXXFLAGS="-g" ./configure --prefix=$basedir/vips &&
+    make &&
+    make install )
+fi
+
+# do the win64 build and package
+if [ -d $linux_install ]; then
+  jhbuild --file=jhbuildrc build --nodeps libvips-$DEPS && \
+    ./package-vipsdev.sh
+fi
---
+++ 8.4/variables.sh
@@ -0,0 +1,33 @@
+# included by all scripts .. edit versions here, and in vips.modules
+
+nip2_package=nip2
+nip2_version=8.0
+
+vips_package=vips
+vips_version=8.4
+vips_minor_version=0
+
+# build-win32/x.xx dir we are building
+basedir=$(pwd)
+
+# download zips to here
+packagedir=packages
+
+# unzip to here
+installdir=inst
+
+# jhbuild will download sources to here 
+checkoutdir=checkout
+
+mingw_prefix=x86_64-w64-mingw32-
+
+repackagedir=$vips_package-dev-$vips_version
+
+# we need a native linux install to pull the typelib from
+# if VIPSHOME is defined, take that, otherwise we're probaly a containerized
+# build, look for it here
+if [ $VIPSHOME ]; then
+	linux_install="$VIPSHOME"
+else
+	linux_install=$basedir/vips
+fi
