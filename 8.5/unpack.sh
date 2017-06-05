515f7942ef1d10352a18669c13621b6aa8217edd
John Cupitt <jcupitt@gmail.com>
2017-06-05 10:11:40 +0100

    better openjpg build
    use -DBUILD_PKGCONFIG_FILES=ON rather than pasting in a .pc file
    
    thanks @bgilbert !

--- 8.5/unpack.sh
+++ 8.5/unpack.sh
@@ -9,6 +9,3 @@ for i in $packagedir/*.zip ; do
 	( cd $installdir ; unzip -o -qq ../$i )
 done
 
-# openjpeg2.1 does not install a .pc file ... copy one in ourselves
-echo installing libopenjp2.pc
-cp libopenjp2.pc inst/lib/pkgconfig
