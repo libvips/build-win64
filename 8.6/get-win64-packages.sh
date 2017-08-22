804e47dadb1bcd2c4a302d6591a1b5b40bd4e730
John Cupitt <jcupitt@gmail.com>
2017-08-22 17:49:44 +0100

    build gettext from source in 8.6

--- 8.6/get-win64-packages.sh
+++ 8.6/get-win64-packages.sh
@@ -20,8 +20,6 @@ while read PACKAGE; do
     echo "I don't know what to do with $PACKAGE as it doesn't match anything I am looking for."
   fi
 done << EOF
-dependencies/gettext-runtime-dev_0.18.1.1-2_win64.zip
-dependencies/gettext-runtime_0.18.1.1-2_win64.zip
 dependencies/zlib_1.2.5-1_win64.zip
 dependencies/zlib-dev_1.2.5-1_win64.zip 
 EOF
