2d5f4c7f9e2a711326c6b0f61760d6f54ecdccbc
John Cupitt <jcupitt@gmail.com>
2018-03-10 18:47:57 +0000

    small cleanups to dockerfile

--- build.sh
+++ build.sh
@@ -21,8 +21,13 @@ docker pull ubuntu:xenial
 # Create a machine image with all the required build tools pre-installed
 docker build -t libvips-build-win64 container
 
-# Run build scripts inside container, with versioned subdirectory mounted at /data
-docker run --rm -t -v $PWD/$VERSION:/data -e "DEPS=$DEPS" libvips-build-win64 sh -c "cp /data/* .; ./build.sh && cp vips-dev-w64-*.zip /data"
+# Run build scripts inside container, with versioned subdirectory 
+# mounted at /data
+docker run --rm -t \
+	-v $PWD/$VERSION:/data \
+	-e "DEPS=$DEPS" \
+	libvips-build-win64 \
+	sh -c ./build.sh
 
 # List result
 ls -al $PWD/$VERSION/*.zip
